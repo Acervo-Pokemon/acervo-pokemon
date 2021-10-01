344c5455b49eb7c11c6a2d414f5d54cc6767ed32
marcospaulo-paixao <contato.marcospaulo1@gmail.com>
2021-09-30 22:46:59 -0300

    Incluindo botão favoritar na flatlist

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,35 +1,54 @@
 // react, react-native, expo
-import React, { useEffect, useState } from 'react'
-import { View, Text, Image } from 'react-native'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-
+import React, { useEffect, useState } from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/FontAwesome";
 // control
-import { getDetails, capitalize } from '../../control/pokemonControl'
+import { getDetails, capitalize } from "../../control/pokemonControl";
 
 // style
-import style from './stylesItem'
-import { useNavigation } from '@react-navigation/native'
+import style from "./stylesItem";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Item({ data }) {
-
-  const [img, setImg] = useState()
-  const navigation = useNavigation()
+  const [img, setImg] = useState();
+  const navigation = useNavigation();
 
   useEffect(() => {
     async function loadImg() {
-      const details = await getDetails(data.url)
-      setImg(details.sprites.front_default)
+      const details = await getDetails(data.url);
+      setImg(details.sprites.front_default);
     }
-    loadImg()
-  })
+    loadImg();
+  });
 
   return (
     <View>
-      <TouchableOpacity style={style.toucheable}
-        onPress={() => navigation.navigate('Details')}>
+      <TouchableOpacity
+        style={style.toucheable}
+        onPress={() => navigation.navigate("Details")}
+      >
         <Image source={{ uri: img }} style={style.image}></Image>
-        <Text style={style.text}>{capitalize(data.name)}</Text>
+        <View style={{width: '50%'}}>
+          <Text style={style.text}>{capitalize(data.name)}</Text>
+        </View>
+        <View style={{ justifyContent: 'center' }}>
+          <ButtonFavorites></ButtonFavorites>
+        </View>
       </TouchableOpacity>
     </View>
-  )
+  );
+}
+
+function ButtonFavorites() {
+  return (
+    <View style={{ }}>
+      <TouchableOpacity
+        onPress={()=> {console.log("favorites")}}
+        style={style.btnFavorites}
+      >
+        <Icon color="grey" size={35} name="star" />
+      </TouchableOpacity>
+    </View>
+  );
 }
